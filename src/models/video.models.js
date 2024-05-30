c8918ea02a7899d8098ce4b080e718cb36cc2922
Graviton19 <aalekhbukhariya@gmail.com>
2024-05-30 18:47:26 +0530

    added user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,43 @@
+import mongoose,{Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String, // Cloudinary url
+            required: true
+        },
+        thumbnail:{
+            type: String, 
+            required: true
+        },
+        owner:{
+            type: Schema.types.ObjectId,
+            ref: "User"
+        },
+        title:{
+            type: String, 
+            required: true
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        duration:{
+            type: Number,
+            required: true
+        },
+        views:{
+            type: Number,
+            default: 0
+        },
+        isPublished:{
+            typr: Boolean,
+            default: true
+        }
+    },{timestamps: true}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video",videoSchema)
